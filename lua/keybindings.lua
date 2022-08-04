c030a57fe16d511a985856e24c937e2a7e6dbe3b
Rodolfo Boer <rodolfo.boer@gmail.com>
2022-08-04 21:24:45 +0200

    hop binding

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,4 +1,4 @@
-local opts = { noremap=true, silent=true, expr = true }
+local opts = { noremap=true, silent=true, expr=true }
 -- Make <CR> to accept selected completion item or notify coc.nvim to format
 vim.keymap.set("i", "<CR>", 
 	"coc#pum#visible() ? coc#pum#confirm() : \"\\<CR>\"", opts)
@@ -13,3 +13,4 @@ vim.keymap.set('n', '<c-e>', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 
+vim.keymap.set('n', 'f', ':HopPattern<CR>')
