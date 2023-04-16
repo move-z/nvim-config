81ddfae3d55aeb5615dbfee42b27b5a118d40d35
Rodolfo Boer <rodolfo.boer@gmail.com>
2023-04-16 21:55:49 +0200

    improve completion

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -2,37 +2,55 @@ return {
   {
     'hrsh7th/nvim-cmp',
     dependencies = {
-      'windwp/nvim-autopairs'
+      'windwp/nvim-autopairs',
+      'onsails/lspkind.nvim',
+      'saecki/crates.nvim',
     },
     config = function()
       -- Completion Plugin Setup
       local cmp = require('cmp')
+      local lspkind = require('lspkind')
       cmp.setup({
         mapping = {
-          ['<C-k>'] = cmp.mapping.select_prev_item(),
-          ['<C-j>'] = cmp.mapping.select_next_item(),
-          -- Add tab support
-          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-          ['<Tab>'] = cmp.mapping.select_next_item(),
+          ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+          ['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-c>'] = cmp.mapping.close(),
           ['<CR>'] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = true,
+            i = function(fallback)
+              if cmp.visible() and cmp.get_active_entry() then
+                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+              else
+                fallback()
+              end
+            end,
+            s = cmp.mapping.confirm({ select = true }),
+            c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
           })
         },
+        preselect = cmp.PreselectMode.None,
         -- Installed sources:
         sources = {
-          { name = 'nvim_lsp' },      -- from language server
-          { name = 'nvim_lsp_signature_help'},            -- display function signatures with current parameter emphasized
-          { name = 'buffer' },        -- source current buffer
+          { name = 'nvim_lsp' },
+          { name = 'nvim_lsp_signature_help'},
+          { name = 'buffer' },
+          { name = 'spell' },
+          { name = 'path' },
+          { name = 'crates' },
         },
         window = {
             completion = cmp.config.window.bordered(),
             documentation = cmp.config.window.bordered(),
         },
+        formatting = {
+          format = lspkind.cmp_format({
+            -- mode = 'symbol', -- show only symbol annotations
+            -- maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            -- ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+          })
+        }
       })
 
       -- If you want insert `(` after select function or method item
@@ -45,7 +63,8 @@ return {
   },
   { 'hrsh7th/cmp-nvim-lsp' },
   { 'hrsh7th/cmp-buffer' },
+  { 'f3fora/cmp-spell' },
+  { 'hrsh7th/cmp-path' },
   { 'hrsh7th/cmp-nvim-lsp-signature-help' },
-
 }
 
