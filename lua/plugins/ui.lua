2f66da3a6cd9e46618dd6822b516ef3a9fc9c11a
Rodolfo Boer <rodolfo.boer@gmail.com>
2023-04-17 00:49:48 +0200

    noice

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -54,5 +54,19 @@ return {
       }
     end
   },
+  -- message ui
+  {
+    "folke/noice.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      -- "rcarriga/nvim-notify",
+    },
+    config = function()
+      require("noice").setup({})
+    end
+  },
 }
 
