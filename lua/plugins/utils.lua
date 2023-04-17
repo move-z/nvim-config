bf6c80d5adf33f18e4cb7cbfbc8f9792f23e439f
Rodolfo Boer <rodolfo.boer@gmail.com>
2023-04-17 19:41:09 +0200

    templates no longer required

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -46,15 +46,6 @@ return {
       end
 
       vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
-
-      local api = require("nvim-tree.api")
-      api.events.subscribe(api.events.Event.FileCreated, function(file)
-        vim.cmd("edit " .. file.fname)
-        local template_dir = "~/.config/nvim/templates/"
-        if (file.fname:match("^.*%.rs$")) then
-          vim.cmd("0read " .. template_dir .. "rust")
-        end
-      end)
     end
   },
   {
--- lua/templates.lua
+++
@@ -1,5 +0,0 @@
-vim.api.nvim_create_autocmd(
-    { "BufNewFile" },
-    { pattern = { "*.rs" }, command = "0r ~/.config/nvim/templates/rust" }
-)
-
