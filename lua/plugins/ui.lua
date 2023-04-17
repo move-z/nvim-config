8e53c7a006191af31b406125d57fb9284edc1641
Rodolfo Boer <rodolfo.boer@gmail.com>
2023-04-17 18:37:52 +0200

    fix noice ui

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -59,10 +59,7 @@ return {
     "folke/noice.nvim",
     dependencies = {
       "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      -- "rcarriga/nvim-notify",
+      "rcarriga/nvim-notify",
     },
     config = function()
       require("noice").setup({
