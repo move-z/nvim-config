c420e418fc21f421cd941e2a9b554f12dd047000
Rodolfo Boer <rodolfo.boer@gmail.com>
2023-04-18 19:49:19 +0200

    fix nvimtree dressing

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -55,7 +55,18 @@ return {
   },
   -- ui select
   {
-    "stevearc/dressing.nvim"
+    "stevearc/dressing.nvim",
+    config = function()
+      require("dressing").setup({
+        input = {
+          get_config = function()
+            if vim.api.nvim_buf_get_option(0, "filetype") == "NvimTree" then
+              return { enabled = false }
+            end
+          end,
+        },
+      })
+    end
   },
   -- highlight current word
   {
