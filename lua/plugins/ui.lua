c8fbfe7f569283dd04fb2c320d34123b70d2a2c9
Rodolfo Boer <rodolfo.boer@previmedical.it>
2023-05-31 10:26:02 +0200

    remove useless config

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -49,18 +49,7 @@ return {
   },
   -- ui select
   {
-    "stevearc/dressing.nvim",
-    config = function()
-      require("dressing").setup({
-        input = {
-          get_config = function()
-            if vim.api.nvim_buf_get_option(0, "filetype") == "NvimTree" then
-              return { enabled = false }
-            end
-          end,
-        },
-      })
-    end
+    "stevearc/dressing.nvim"
   },
   -- highlight current word
   {
